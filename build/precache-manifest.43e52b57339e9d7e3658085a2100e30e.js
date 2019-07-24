2a2ef4dc23f45071ada4d2c78216d750cd147fed
AgnLiska <agiusia@gmail.com>
2019-07-24 11:39:54 +0200

    introducing robofriends

---
+++ build/precache-manifest.43e52b57339e9d7e3658085a2100e30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811bf8ca93ff8f8a908603883c6b84c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ac1ca618982732f4e18c",
+    "url": "/static/css/main.cad34f49.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "ac1ca618982732f4e18c",
+    "url": "/static/js/main.e2ba8e25.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
